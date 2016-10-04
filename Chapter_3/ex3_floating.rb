47b9df49855d45665ba145a8d2e0694e02a30831
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-04 11:07:05 -0600

    Updated four files in Chapter 3
    * Updated Readme.md with observations on calculating in ruby and
      the names, symbols, and definitions of basic operators
    * I updated ex3.rb with the information from the text, tested and
      ran it successfully. I added comments prior to each line explaing
      what each line does.
    * I updated ex3.floating.rb with floating point values rather than
      integers.
    * I did some calculations in irb and added those to irb_calculator
    
    [#131482361]

--- Chapter_3/ex3_floating.rb
+++ Chapter_3/ex3_floating.rb
@@ -1,38 +1,27 @@
-# + Plus : Does addition.
-# - Minus : Does subtraction.
-# / Slash : Does division.
-# * Asterisk: Does multiplication.
-# % Percent (Modulus) : Does divison, returns only the remainder.
-# < Less than : compares to check for less than.
-# > Greater than: compares to check of greater than.
-# <= Less than or equal to: compares to check for less than or equal to value.
-# >= Greater than or equal to: compares to check for greater than or equal
-# to value.
-
 # writes the string to the screen
 puts 'I will now count my chickens:'
 # writes Hens and calculates and prints the value 30
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 # writes Roosters and calculates and prints the value 97
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # writes the string to the screen
 puts 'Now I will count the eggs:'
 # calculates and prints the value to the screen (7)
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # writes the string to the screen
 puts 'Is it true that 3 + 2 < 5 - 7?'
 # calculates, does a comparison and prints a true false (false) to
 # the screen
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # writes the string, calculates, and writes the value as part of the
 # string.
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
 # writes the string, calculates, and writes the value as part of the
 # string.
-puts "What is 5-7? #{5 - 7}"
+puts "What is 5-7? #{5.0 - 7.0}"
 
 # writes the string to the screen
 puts 'Oh, that\'s why it\'s false.'
@@ -42,10 +31,10 @@ puts 'How about some more?'
 
 # writes the string, calculates, compares and includes the true false
 # as a part of the string.
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2.0}"
 # writes the string, calculates, compares and includes the true false
 # as a part of the string.
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
 # writes the string, calculates, compares and includes the true false
 # as a part of the string.
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
