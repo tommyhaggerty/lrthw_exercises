ef106e1f7fb5151f9a8f4d07ea07e53e2641faa8
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-05 14:12:50 -0600

    Updated ex_5 and my_ex5.rb
    * I updated ex5.rb with the correct code. Correcting errors from
      my initial input.
    
    [#131482365]

--- Chapter 5/my_ex5.rb
+++ Chapter 5/my_ex5.rb
@@ -7,10 +7,10 @@ my_teeth = 'White'
 my_hair = 'Brown'
 
 puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inchest tall."
+puts "He's #{my_height} inches tall."
 puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair}."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
