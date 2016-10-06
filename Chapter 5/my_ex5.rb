0c794bd20e941d896268c9986affb843e916b3b3
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-05 18:42:33 -0600

    Updated my_ex5.rb
    * I added comments to explain which are the variables, including
      the two new variables to convert weight and height.
    
    [#131482365]

--- Chapter 5/my_ex5.rb
+++ Chapter 5/my_ex5.rb
@@ -1,3 +1,5 @@
+# The following are variables. The initial program had the my_ prefix on each one
+# The exercise required those to be removed so that has been done.
 name = 'Zed A. Shaw'
 age = 35 # not a lie in 2009 if you are zed shaw
 height = 74 # inches
@@ -5,10 +7,12 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
-
+# The following two variables I added to convert height from inches to cm
+# and weight from lbs to kilograms.
 height_cm = height * 2.54
 weight_kg = weight * 0.45359237
 
+# Here is where we display all the information stored in the variables.
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
 puts "That's #{height_cm} in cm."
