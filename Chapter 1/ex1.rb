961b94a4451cec4124eaec8c11d8838b360c6fbe
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-16 17:00:32 -0600

    Updated Chapter 1
    * Replaced the line that had escape characters single quotes with
      double quotes and removed the escape characters.
    
    [#131482357]

--- Chapter 1/ex1.rb
+++ Chapter 1/ex1.rb
@@ -3,6 +3,6 @@ puts 'Hello Again'
 puts 'I like typing this.'
 puts 'This is fun.'
 puts 'Yay! Printing.'
-puts 'I\'d much rather you \'not\'.'
+puts "I'd much rather you 'not'."
 # puts 'I "said" do not touch this.'
 puts 'This line is part of the exercise one study drills.'
