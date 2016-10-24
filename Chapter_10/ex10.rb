eb8c54371e7d3418d68ca170e1ef2bf2c3463033
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-24 09:57:14 -0600

    Updated Chapter 10
    * Added code from the book lrthw.
    * I learned about escape sequences
    * I also added info to the Readme.md
    
    [#131482375]

---
+++ Chapter_10/ex10.rb
@@ -0,0 +1,24 @@
+tabby_cat = '\tI\'m tabbed in.'
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+\aI'll do a \flist:
+\t* Cat \rfood
+\t* \"Fishies\"\v
+\t* Catnip\n\t* \bGrass
+\x16 \016
+"""
+
+fat_cats = '''
+Can you type
+Multiple
+Lines this way?
+'''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts fat_cats
+
